cada1438f78d63386d2b746d19c403b22c721b70
Shahdelaraby <shahdelaraby36@gmail.com>
2024-10-17 23:57:36 +0300

    message commit

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\MyCustomServiceProvider::class,
+   // App\Providers\MyCustomServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
 ];
